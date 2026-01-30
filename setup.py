15c98c3d697d861714b5e1d6a2c8f7111afc5da6
Joy Kumar Acharjee <joy29940@gmail.com>
2026-01-30 22:14:58 +0600

    feat: added initial file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+def get_requirements(file_path: str) -> list[str]:
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = [line.strip() for line in file.readlines()]
+
+    return requirements
+
 setup(
     name='student-performance',
     version='1.0.0',
     author='Joy Kumar Acharjee',
     author_email='joy29940@gmail.com',
     packages=find_packages(),
-    install_requires=['matplotlib', 'numpy']
+    install_requires=get_requirements('requirements.txt')
 )
