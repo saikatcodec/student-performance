2208f4922744445ac4239686f76a40ccb9546ef2
Joy Kumar Acharjee <joy29940@gmail.com>
2026-01-30 20:05:51 +0600

    feat: implementing setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='student-performance',
+    version='1.0.0',
+    author='Joy Kumar Acharjee',
+    author_email='joy29940@gmail.com',
+    packages=find_packages(),
+    install_requires=['matplotlib', 'numpy']
+)
