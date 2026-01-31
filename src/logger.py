cf93792893350c699aef5b0f2b63f8f2e4dac09a
Joy Kumar Acharjee <joy29940@gmail.com>
2026-01-31 15:09:13 +0600

    feat: fixed logger problem

--- src/logger.py
+++ src/logger.py
@@ -9,7 +9,7 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE = os.path.join(logs_path, LOG_FILE_PATH + '.log')
 
 logging.basicConfig(
-    format='[%(asctime)s] - %(name)s - %(levelname)s - %(message)s',
+    format='[ %(asctime)s ] - %(lineno)d - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S %p',
     level=logging.INFO,
     filename=LOG_FILE
